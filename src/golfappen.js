0f518fe528d11274337794b7ed83e204be97da61
Ida Alin <dev.7rip7ych@gmail.com>
2026-06-26 19:10:36 +0200

    Update golfappen

--- src/golfappen.js
+++ src/golfappen.js
@@ -5,3 +5,11 @@ const navLinks = document.getElementById('nav-links')
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('show')
 })
+
+function loadPlayTypes() {
+    console.log(1)
+}
+
+function loadGolfCourses() {
+    console.log(1)
+}
